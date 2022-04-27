9ef2375e9afa81c18c3cd22f8f48cf10c7d80d0b
RathogwaInnocent <innocentrathogwa@gmail.com>
2022-04-27 21:34:10 +0200

    Added better testing assertions

--- cypress/integration/automation-test-store/contact-us.js
+++ cypress/integration/automation-test-store/contact-us.js
@@ -4,12 +4,14 @@ describe("Test Contact Us form via Automation Test Store", () =>{
     it("Should be able to submit a sucessessful submission via conatc us from", () => {
         
         cy.visit("https://www.automationteststore.com")
-        //cy.get('#contact-us > .thumbnail').click({force: true})
-        cy.get('[name="first_name"]').type("Innocent")
-        cy.get('[name="last_name"]').type("Rathogwa")
-        cy.get('[name="email"]').type("innrat004@gmail.com")
-        cy.get('textarea.feedback-input').type("I can I enroll into your course")
-        cy.get('[type="submit"]').click();
+        cy.xpath("//a[contains(@href, 'contact')]").click();
+        //cy.get("a[href$='contact']").click;
+        cy.get('#ContactUsFrm_first_name').type("Innocent")
+        cy.get('#ContactUsFrm_email').type("innrat004@Rathogwa.com")
+        cy.get('#ContactUsFrm_email').should('have.attr','name', 'email')
+        cy.get('#ContactUsFrm_enquiry').type("Do you provide additional discount on bulk orders?")
+        cy.get("button[title='Submit']").click();
+        cy.get('.mb40 > :nth-child(3)').should('have.text','Your enquiry has been successfully sent to the store owner!')
     });
 
 })
